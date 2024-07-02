089ec73509055ea3968d1942cccdbe4da623a199
Zeeshan Ali Khan <zeenix@gmail.com>
2024-07-02 22:53:47 +0200

    impl<T: Type> DynamicType for T

--- static-dbus-signature/src/dynamic_type.rs
+++ static-dbus-signature/src/dynamic_type.rs
@@ -1,5 +1,14 @@
-use crate::signature::Signature;
+use crate::{r#type::Type, signature::Signature};
 
 pub trait DynamicType {
     fn signature(&self) -> Signature<'_>;
+}
+
+impl<T> DynamicType for T
+where
+    T: Type,
+{
+    fn signature(&self) -> Signature<'_> {
+        T::SIGNATURE
+    }
 }
