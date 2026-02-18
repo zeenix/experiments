7925d629f9ec3ca930369a9bedcbdda4851e2630
Zeeshan Ali Khan <zeenix@gmail.com>
2026-02-18 18:49:08 +0000

    runtime: Remove redundant code

--- zruntime/src/main.rs
+++ zruntime/src/main.rs
@@ -1,29 +1,10 @@
-use std::{
-    future::Future,
-    pin::Pin,
-    str::from_utf8,
-    task::{Context, Poll},
-};
+use std::str::from_utf8;
 
 mod executor;
 mod unix_stream;
 
 use executor::{naive, smarter, Executor, TaskHandle};
 use unix_stream::{naive as naive_unix, smarter as smarter_unix, UnixStream};
-
-struct MyFuture(u32);
-
-impl Future for MyFuture {
-    type Output = u32;
-
-    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
-        Poll::Ready(self.0)
-    }
-}
-
-async fn give_me_u32() -> u32 {
-    MyFuture(42).await
-}
 
 fn main() {
     println!("Running smarter executor..");
@@ -52,10 +33,6 @@ where
         let written = tx.write(msg).await.unwrap();
         assert_eq!(written, msg.len());
     });
-
-    executor.block_on(async move {});
-    let num = executor.block_on(give_me_u32());
-    println!("\tReceived number: {}", num);
 
     executor.run();
 
